f4332674e34acc3eb36db00eb3c7718ae39ddf03
Serega-IT <137938526+Serega-IT@users.noreply.github.com>
2023-07-09 13:43:58 +0200

    Add files via upload

---
+++ Tel - Ran/HomeWork1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String...args) {
+        String text = "Hello World";
+        int number = 22;
+
+        System.out.println(text);
+        System.out.println(number);
+    }
+}
---
+++ Tel - Ran/HomeWork2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String...args) {
+        int number1 = 16;
+        int number2 = 8;
+        int number3 = number1 + number2;
+        int number4 = number1 - number2;
+        int number5 = number1 * number2;
+        int number6 = number1 / number2;
+        System.out.println(number3 + "\n" +  number4 + "\n" + number5 + "\n" + number6);
+    }
+}
