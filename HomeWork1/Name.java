66744dc5003d5a09da8a050a13001f78b30aef89
Serega-IT <137938526+Serega-IT@users.noreply.github.com>
2023-07-23 18:43:04 +0200

    Add files via upload

---
+++ HomeWork1/Name.java
@@ -0,0 +1,17 @@
+package HomeWork1;
+
+import java.util.Scanner;
+
+public class Name {
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+        System.out.print("Привет: имя пользователя!");
+        String name = scanner.nextLine();
+        System.out.println("Ваше имя: " + name);
+
+        // Не смог реализовать чтобы каретка была на месте имя пользователя
+        // Philip как это можно реализовать?
+
+    }
+}
